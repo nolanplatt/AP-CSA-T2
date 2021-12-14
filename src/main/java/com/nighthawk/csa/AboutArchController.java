47484dfef9c07bdf94283d18d5b4cf9df43f4ff6
Arch Huang <ArchKitten05@gmail.com>
2021-12-14 10:08:46 -0800

    Imported aboutArch.html and AboutArchController.java

---
+++ src/main/java/com/nighthawk/csa/AboutArchController.java
@@ -0,0 +1,19 @@
+package com.nighthawk.csa;
+
+import com.nighthawk.csa.starters.ImageInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AboutArchController {
+
+    @GetMapping("/aboutArch")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String aboutArch() {
+        return "aboutArch";
+    }
+}
