7036397d8154599321aeaa75c2b30161999b17d3
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-12-15 08:24:53 -0800

    fix issue preventing runtime from running

--- src/main/java/com/nighthawk/csa/Main.java
+++ src/main/java/com/nighthawk/csa/Main.java
@@ -2,11 +2,11 @@ package com.nighthawk.csa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        System.out.println("Done!");
     }
 }
