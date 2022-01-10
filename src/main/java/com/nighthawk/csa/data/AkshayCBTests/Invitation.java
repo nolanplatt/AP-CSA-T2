a8cc5e76aad0216c72bf03de2b487d1bd21620ce
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2022-01-10 09:09:07 -0800

    Invitation FRQ completed, with one-parameter constructor completed

--- src/main/java/com/nighthawk/csa/data/AkshayCBTests/Invitation.java
+++ src/main/java/com/nighthawk/csa/data/AkshayCBTests/Invitation.java
@@ -4,9 +4,14 @@ public class Invitation {
     private String hostName;
     private String address;
 
-    public Invitation(String n, String a) {
-        hostName = n;
-        address = a;
+//    public Invitation(String n, String a) {
+//        hostName = n;
+//        address = a;
+//    }
+
+    public Invitation(String address) {
+        this.address = address;
+        hostName = "Host";
     }
 
     public String getHostName() {
@@ -16,5 +21,11 @@ public class Invitation {
     public void setAddress(String a) {
         address = a;
     }
+
+    public String invitationString(String invitee) {
+        String inviteString = "Dear " + invitee + ", please attend my event at " + address + ". See you then, " + hostName + ".";
+        return inviteString;
+    }
+
 
 }
