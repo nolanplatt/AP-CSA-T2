dbc1394dccdbb20113b2958e89d27a9cc215137b
Chris-Ru <christopherrubin68@gmail.com>
2022-02-15 10:05:42 -0800

    Number rework

--- src/main/java/com/nighthawk/csa/ChrisNumber.java
+++ src/main/java/com/nighthawk/csa/ChrisNumber.java
@@ -1,36 +1,31 @@
 package com.nighthawk.csa;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 // Write a Class Number
 public class ChrisNumber {
-    private int min = 3;
-    private int max = 36;
+    // instance variables
+    private int min=3;
+    private int max=36;
 
     // Number has a zero Argument constructor
     // It initializes a random number between 3 and 36, ie the number of squirrels in class
-    public ChrisNumber(int n, int mi, int ma) {
+    public ChrisNumber(int min, int max) {
         // constructor
-        // instance variables
-        min = mi;
-        max = ma;
+        this.min = min;
+        this.max = max;
     }
 
     // It contains a getter for the Random Number
-    public int min() {
-        return this.min;
+    public double rand(){
+        return Math.random() * ( max - min );
     }
-
-    public int max() {
-        return this.max;
-    }
-
 
 
     // It contains a getter for Index, or the order it was initialized
+    public Index() {
 
-
+    }
 
     // Write a tester method
     public static void main(String[] args) {
@@ -40,16 +35,12 @@ public class ChrisNumber {
 
         // Initialize 10 squirrels of class type Number
         // Insert Number instance into ArrayList Squirrel in least to greatest order by random number, mine required nested loops
-        squirrels.add(3);
-        squirrels.add(8);
-        squirrels.add(13);
 
 
         // Print a formatted message with number of Squirrels and Index by which they were created, use enhanced for loop
-        for(number : n) {
-            System.out.println();
-        }
+        for( : ) {
 
+        }
     }
 
 }
