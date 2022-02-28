631a14f8257065985a4cc27902c2820efb8ae5e8
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2022-02-27 18:46:37 -0800

    Extremely basic person list

--- src/main/java/com/nighthawk/csa/PlayersController.java
+++ src/main/java/com/nighthawk/csa/PlayersController.java
@@ -1,18 +1,21 @@
 package com.nighthawk.csa;
 
-import com.nighthawk.csa.data.SQL.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @Controller
-public class ProfileController {
+public class PlayersController {
+    @GetMapping("/players/list")
+    public String ProfileView() {
+        return "players";
+    }
+
     @GetMapping("/player/{id}")
     public String ProfileView(@PathVariable("id") long id, Model model) {
         model.addAttribute("id", id);
         return "profile";
     }
+
 }
